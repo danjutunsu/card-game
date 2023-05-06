74b762ce32c73696b7e04d38854e81d9fd426dbd
Daniel <danjwetzel@gmail.com>
2023-05-05 21:59:38 -0700

    working on changing turn on finish answering

--- src/Card.tsx
+++ src/Card.tsx
@@ -33,7 +33,6 @@ const Card: React.FC = () => {
 
     useEffect(() => {
         async function fetchData() {
-            // console.log("WORKING")
             const response = await fetch(`${url}/api/questions`);
             const jsonData = await response.json();
             setData(jsonData);
@@ -87,23 +86,44 @@ const Card: React.FC = () => {
     } 
       
     function getRandomQuestion() {
-        let index = Math.floor(Math.random() * data.length);
-        while (visited.includes(index)) {
-            index = Math.floor(Math.random() * data.length);
+        if (visited.length === data.length) {
+          // all questions have been visited
+          console.log("Finished Guessing")
+          getUserPoints(parseInt(userId))
+          navigate('/lobby');
+          return null; // return the current question
         }
+      
+        let index = Math.floor(Math.random() * data.length);
+      
+        while (visited.includes(index)) {
+          index = Math.floor(Math.random() * data.length);
+          console.log('while')
+          if (visited.length === data.length) {
+            // all questions have been visited
+            getUserPoints(parseInt(userId))
+            return index; // return the last index
+          }
+        }
+      
+        console.log("WORKS")
         setRandomQuestion(index);
         setVisited([...visited, index]);
-        if (visited.length === data.length) 
-        {
-            // all questions have been visited
-            console.log("Finished Guessing")
-            getUserPoints(parseInt(userId))
-            navigate('/stats');
-            return null; // return the current question
-        }
-    
+        console.log(`Visited: ${visited.length}`)
+        console.log(`Data: ${data.length}`)
+      
         return index;
-    }
+      }
+
+    const nextRound = async (currentUser: number) => {
+        try {
+            const response = await axios.post(`${url}/api/round`, {
+                currentUser: currentUser
+            })
+        } catch (err) {
+
+        }
+    }      
 
     const addGuess = async (userId: number, questionId: number, userGuess: number) => {
         try {
@@ -112,6 +132,30 @@ const Card: React.FC = () => {
             questionId: questionId,
             userGuess: userGuess
           });
+          console.log(response.data); // handle the response from the backend
+        } catch (error) {
+          console.error(error);
+        }
+    } 
+
+    const addAnswer = async (userId: number, questionId: number, answer: number, answered: number, count: number) => {
+        try {
+          const response = await axios.post(`${url}/api/answers`, {
+            userId: userId,
+            questionId: questionId,
+            answer: answer,
+            answered: answered,
+            count: count
+          });
+          if (answered === data.length) {
+            await axios.put(`${url}/api/games`, {
+                userId: userId,
+                questionId: questionId,
+                answer: answer,
+                answered: answered,
+                count: count
+              });
+          }
           console.log(response.data); // handle the response from the backend
         } catch (error) {
           console.error(error);
@@ -147,23 +191,31 @@ const Card: React.FC = () => {
         }
     } 
       
-    function handleNextQuestion(index: number) {
+    function handleAnswerNextQuestion(index: number) {
         const questionId = data[randomQuestion].id;
-        const userGuess = index;
-        addGuess(parseInt(userId), questionId, userGuess);
-        if (index === data[randomQuestion].answer)
-        {
-            console.log("CORRECT")
-            setCorrectlyAnswered(correctlyAnswered+1)
-            getRandomQuestion();
-        }
-        else
-        {
-            console.log("INCORRECT")
-            getRandomQuestion();
-        }
+        const userAnswer = index;
+        addAnswer(parseInt(userId), questionId, userAnswer, answered, data.length);
         setAnswered(answered+1)
+        getRandomQuestion();
     }
+
+    // function handleNextQuestion(index: number) {
+    //     const questionId = data[randomQuestion].id;
+    //     const userGuess = index;
+    //     addGuess(parseInt(userId), questionId, userGuess);
+    //     if (index === data[randomQuestion].answer)
+    //     {
+    //         console.log("CORRECT")
+            // setCorrectlyAnswered(correctlyAnswered+1)
+    //         getRandomQuestion();
+    //     }
+    //     else
+    //     {
+    //         console.log("INCORRECT")
+    //         getRandomQuestion();
+    //     }
+    //     setAnswered(answered+1)
+    // }
     
     return (
         <div className="card-page">
@@ -173,7 +225,7 @@ const Card: React.FC = () => {
                         {data[randomQuestion].options.map((option, index) => (
                             <div key={index}>
                                 <div className="button-container">
-                                    <button className="button" onClick={() => handleNextQuestion(index)}>{option}</button>
+                                    <button className="button" onClick={() => handleAnswerNextQuestion(index)}>{option}</button>
                                 </div>
                             </div>
                         ))}
