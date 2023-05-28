d025ef0945fa8bb90e1c3c0f298907924b88bafb
danjutunsu <33231608+danjutunsu@users.noreply.github.com>
2023-05-28 15:45:56 -0700

    changed url to backend on netlify

--- src/Config.tsx
+++ src/Config.tsx
@@ -1,4 +1,4 @@
 // config.js
 
-export const SERVER_PORT = 3001;
-export const url = `https://elaborate-blancmange-554bcc.netlify.app:${SERVER_PORT}`
+// export const SERVER_PORT = 3001;
+export const url = `https://backend-elaborate-blancmange-554bcc.netlify.app`
