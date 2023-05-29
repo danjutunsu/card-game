0cdce2f6567acb5fe21f134c437eebb672368b3a
danjutunsu <33231608+danjutunsu@users.noreply.github.com>
2023-05-28 23:17:00 -0700

    remove port

--- src/Config.tsx
+++ src/Config.tsx
@@ -1,4 +1,4 @@
 // config.js
 
-export const SERVER_PORT = 3001;
-export const url = `https://triviafriends.herokuapp.com:${SERVER_PORT}`
+// export const SERVER_PORT = 3001;
+export const url = `https://triviafriends.herokuapp.com`
