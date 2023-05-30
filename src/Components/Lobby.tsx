e2a3fe32e18e3803e737517a8d74933cf9bea83b
danjutunsu <33231608+danjutunsu@users.noreply.github.com>
2023-05-30 11:40:21 -0700

    test

--- src/Components/Lobby.tsx
+++ src/Components/Lobby.tsx
@@ -158,7 +158,7 @@ const Lobby = () => {
   // const [ip, setIp] = useState('127.0.0.1')
 
   //create new socket
-  const socket = new WebSocket(`ws://triviafriends.herokuapp.com?userId=${userId}`)
+  const socket = new WebSocket(`wss://triviafriends.herokuapp.com?userId=${userId}`)
 
   // getIp();
 
