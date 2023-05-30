73598aa9a990db04e8e0acbacddf34f7e5a936cd
danjutunsu <33231608+danjutunsu@users.noreply.github.com>
2023-05-30 09:50:29 -0700

    websocket

--- src/Components/Lobby.tsx
+++ src/Components/Lobby.tsx
@@ -158,7 +158,7 @@ const Lobby = () => {
   // const [ip, setIp] = useState('127.0.0.1')
 
   //create new socket
-  const socket = new WebSocket(`ws://127.0.0.1:3002?userId=${userId}`)
+  const socket = new WebSocket(`wss://localhost:3002?userId=${userId}`)
 
   // getIp();
 
