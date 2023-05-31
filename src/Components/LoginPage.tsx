92fa96b4ff38a65b4c960b340441902739f62e3f
danjutunsu <33231608+danjutunsu@users.noreply.github.com>
2023-05-31 15:17:52 -0700

    test

--- src/Components/LoginPage.tsx
+++ src/Components/LoginPage.tsx
@@ -30,7 +30,7 @@
     console.log(`RandomID: ${randomId}`)
 //
     try {
-      const response = await axios.post(`${url}/api/login`, {
+      const response = await axios.post(`${url}/login`, {
         username: username,
         password: password,
       });
@@ -68,7 +68,7 @@
 
   const handleUpdateUUID = async (id: string, uuid: string | undefined) => {
     try {
-      const response = await axios.put(`${url}/api/lobby/uuid`, {
+      const response = await axios.put(`${url}/lobby/uuid`, {
         id: id,
         uuid: uuid
       });
