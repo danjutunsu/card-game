fa4cddf98f084fe66a5c1cc1c477bfda03acdb43
danjutunsu <33231608+danjutunsu@users.noreply.github.com>
2023-06-04 23:04:37 -0700

    test

--- src/App.tsx
+++ src/App.tsx
@@ -28,7 +28,7 @@ function App() {
 
   const getUserPoints = async (uid: number) => {
     try {
-        const response = await axios.get(`${url}/points`, {
+        const response = await axios.get(`${process.env.BACKEND_URL}/points`, {
             params: 
             {
                 userId: uid,
