126cf1c7a6712fa27ce950eeb325cb5f3aad138b
danjutunsu <33231608+danjutunsu@users.noreply.github.com>
2023-06-04 22:46:28 -0700

    changed websocket address

--- src/Components/WebSocketService.tsx
+++ src/Components/WebSocketService.tsx
@@ -1,7 +1,7 @@
 let socket: WebSocket;
 
 export function initializeWebSocket(userId: string) {
-const socket = new WebSocket(`wss://triviafriends.herokuapp.com?userId=${userId}`);
+    const socket = new WebSocket(`wss://triviafriendsserver.onrender.com/?userId=${userId}`)
 
 // Add event listeners and handle WebSocket events here
 
